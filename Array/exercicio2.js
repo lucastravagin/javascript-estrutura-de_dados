8543659a43cc44137ebdedab57472b3eca1846ec
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-11 11:53:02 -0300

    Exercicio 02 concluído

--- Array/exercicio2.js
+++ Array/exercicio2.js
@@ -23,7 +23,23 @@ for(let i = 0; i < alunosDaTurma.length; i++) {
 for(let array in alunosDaTurma) {
     let nota = Math.round((Math.random() * 10))
     console.log(`Aluno ${array} nota ${nota}`);
-    mediaDaSala = (mediaDaSala + nota)
+    mediaDaSala += nota
 }
 
 console.log(mediaDaSala / numeroDeAlunos)
+
+// var alunos =  parseInt(prompt("Numero de alunos: "));
+//  var vetor = new Array(alunos);
+
+//  // Preenchendo o veotr
+//  for(let count=0; count<alunos; count++)
+//   vetor[count] = parseInt( prompt("Aluno " + (count+1) + ":") )
+ 
+
+//  // Somando todos os elementos
+//  var soma=0;
+//  for(let count in vetor)
+//   soma += vetor[count];
+
+//  var media = soma/alunos;
+//  document.write("Média da turma: " + media);
