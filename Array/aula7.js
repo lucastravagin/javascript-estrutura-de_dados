8ebde88cf7a776266ea7bd1c907d74fee9b5e88f
oursys <admin@oursys.com.br>
2020-04-10 21:03:30 -0300

    Aula 07 concluída

---
+++ Array/aula7.js
@@ -0,0 +1,11 @@
+//Arays Bidimensionais
+
+
+let avgTempWeek = []
+let avgTempWeek1 = [25,36,53,25,23,33]
+let avgTempWeek2 = [25,36,53,25,23,33]
+
+avgTempWeek[0] = avgTempWeek1
+avgTempWeek[1] = avgTempWeek2
+
+console.log(avgTempWeek[0][2]);
