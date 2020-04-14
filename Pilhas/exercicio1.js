fe925946db1712523ae9fee2f1eaed41761c4899
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-04-14 17:34:59 -0300

    Exercicio 1 Pilha de Pilha concluído

--- Pilhas/exercicio1.js
+++ Pilhas/exercicio1.js
@@ -1,3 +1,23 @@
 //Algorítmos manipulam dados e quando estes dados estão organizados de forma
 //coerente caracterizam uma estrutura de dados
 
+//Exercicios Decimal para binário
+
+
+let dec2Bin = (decNumber) => {
+    let restStack = [],
+    rest,
+    binaryString = ''
+
+    while(decNumber > 0 ) {
+        rest = Math.floor(decNumber % 2)
+        restStack.push(rest)
+        decNumber = Math.floor(decNumber / 2)
+    }
+
+    while(restStack.length > 0) {
+        binaryString += restStack.pop().toString()
+    }
+
+    return binaryString
+}   
